152861c7a8ddc9b32e4480b94067f08d9e280640
Ramya <remyat@kh.amrita.edu>
2026-09-25 09:52:46 +0000

    fix: completely remove broken template placeholder file

--- src/test/java/YOUR_PATH_HERE.java
+++
@@ -1,5 +0,0 @@
- @Test
-    void intentionalPipelineFailureTest() {
-        // This will deliberately fail because 1 is not equal to 2
-        org.junit.jupiter.api.Assertions.assertEquals(1, 2, "Deliberate pipeline failure execution trace.");
-    }
